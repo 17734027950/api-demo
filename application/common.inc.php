a919243fcf7ab8f5a775ac03df7b1872e6d39c3c
17734027950 <17734027950@qq.com>
2017-12-29 17:33:35 +0800

    '1.1.0'

---
+++ application/common.inc.php
@@ -0,0 +1,18 @@
+<?php
+//全站配置文件
+define("CMS_WEBNAME","微信小程序测试"); // 网站名称
+define("CMS_BASEHOST",""); // 站点根网址
+define("CMS_UPLOADS","/uploads"); // 图片/上传文件默认路径
+define("CMS_CSS","/css"); // css默认存放路径
+define("CMS_JS","/js"); // js默认存放路径
+define("CMS_INDEXNAME","首页"); // 主页链接名
+define("CMS_POWERBY","©微信小程序测试"); // 网站版权信息
+define("CMS_IMGWIDTH","240"); // 缩略图默认宽度
+define("CMS_IMGHEIGHT","180"); // 缩略图默认高度
+define("CMS_SEOTITLE","demo"); // 网站seotitle
+define("CMS_KEYWORDS","demo"); // 网站关键词Keywords
+define("CMS_DESCRIPTION","demo"); // 网站描述
+define("CMS_ISCACHE","0"); // 是否开启缓存，1开启，0关闭
+define("CMS_MAXARC","220"); // 最大返回列表结果数，默认220
+define("CMS_PAGESIZE","2"); // 列表每页显示的数量，默认11
+?>
